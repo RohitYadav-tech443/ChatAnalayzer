94caaa766009e4e67825d29f6cc29bdcc6595987
Rohit Yadav <rohit202824@gmail.com>
2026-08-28 22:40:52 +0530

    Fix regex warning

--- preprocessor.py
+++ preprocessor.py
@@ -23,7 +23,7 @@ def preprocess(data):
     users = []
     messages = []
     for message in df['user_message']:
-        entry = re.split('(r[\w\W]+?):\s', message)
+        entry = re.split(r'([\w\W]+?):\s', message)
         if entry[1:]:
             users.append(entry[1])
             messages.append(entry[2])
